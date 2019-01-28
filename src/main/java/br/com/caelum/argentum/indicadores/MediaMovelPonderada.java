0c01285a5741afd7e6841993278733244a27c43a
severino-on <43893211+severino-on@users.noreply.github.com>
2019-01-28 15:43:17 -0200

    Update MediaMovelPonderada.java

--- src/main/java/br/com/caelum/argentum/indicadores/MediaMovelPonderada.java
+++ src/main/java/br/com/caelum/argentum/indicadores/MediaMovelPonderada.java
@@ -6,7 +6,7 @@ public class MediaMovelPonderada implements Indicador{
 
 	private Indicador outroIndicador;
 
-	public MediaMovelPonderada_(Indicador outroIndicador) {
+	public MediaMovelPonderada(Indicador outroIndicador) {
 		this.outroIndicador = outroIndicador;
 	}
 	
